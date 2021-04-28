b1345392393525d491318284b62ace6c62ebe106
auxb <auxence.blondel@gmail.com>
2021-04-28 12:47:36 +0200

    build(precommit): configure lint-staged

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -9,6 +9,7 @@ function App(): JSX.Element {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
