63f7bda2aa249c8ff2b09b3ffa9ff8687b3dd000
yacinemehai <yacine.mehai@gmail.com>
2021-04-29 14:32:35 +0200

    feat(sidebar): wip drawer notifications

---
+++ client/src/components/Notification.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Toast } from '@blueprintjs/core';
+
+function Notification() {
+  return <Toast message="test toast" />;
+}
+export default Notification;
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,226 +1,35 @@
-//import React from 'react';
+import React from 'react';
+import { Drawer } from '@blueprintjs/core';
+import Notification from './Notification';
 
-//function Sidebar({}) {
-//return <p>hello lucas</p>;
-//}
-
-//export default Sidebar;
-
-import * as React from 'react';
-
-import {
-  Button,
-  Classes,
-  Code,
-  Divider,
-  Drawer,
-  H5,
-  HTMLSelect,
-  OptionProps,
-  Label,
-  Position,
-  Switch,
-} from '@blueprintjs/core';
-import {
-  Example,
-  handleBooleanChange,
-  handleStringChange,
-  handleValueChange,
-  IExampleProps,
-} from '@blueprintjs/docs-theme';
-
-import { IBlueprintExampleData } from '../../tags/types';
-
-export interface IDrawerExampleState {
-  autoFocus: boolean;
-  canEscapeKeyClose: boolean;
-  canOutsideClickClose: boolean;
-  enforceFocus: boolean;
-  hasBackdrop: boolean;
-  isOpen: boolean;
-  position?: Position;
-  size: string;
-  usePortal: boolean;
-}
-export class DrawerExample extends React.PureComponent<
-  IExampleProps<IBlueprintExampleData>,
-  IDrawerExampleState
-> {
-  public state: IDrawerExampleState = {
-    autoFocus: true,
-    canEscapeKeyClose: true,
-    canOutsideClickClose: true,
-    enforceFocus: true,
-    hasBackdrop: true,
-    isOpen: false,
-    position: Position.RIGHT,
-    size: undefined,
-    usePortal: true,
+function Sidebar() {
+  const user = {
+    firstName: 'Jake',
+    lastName: 'Mike',
+    classroom: 'WnS - Remote',
+    picture: 'https://randomuser.me/api/portraits/men/80.jpg',
   };
-
-  private handleAutoFocusChange = handleBooleanChange((autoFocus) =>
-    this.setState({ autoFocus })
-  );
-
-  private handleBackdropChange = handleBooleanChange((hasBackdrop) =>
-    this.setState({ hasBackdrop })
-  );
-
-  private handleEnforceFocusChange = handleBooleanChange((enforceFocus) =>
-    this.setState({ enforceFocus })
-  );
-
-  private handleEscapeKeyChange = handleBooleanChange((canEscapeKeyClose) =>
-    this.setState({ canEscapeKeyClose })
-  );
-
-  private handleUsePortalChange = handleBooleanChange((usePortal) =>
-    this.setState({ usePortal })
-  );
-
-  private handlePositionChange = handleValueChange((position: Position) =>
-    this.setState({ position })
-  );
-
-  private handleOutsideClickChange = handleBooleanChange((val) =>
-    this.setState({ canOutsideClickClose: val })
-  );
-
-  private handleSizeChange = handleStringChange((size) =>
-    this.setState({ size })
-  );
-
-  public render() {
-    return (
-      <Example options={this.renderOptions()} {...this.props}>
-        <Button onClick={this.handleOpen}>Show Drawer</Button>
-        <Drawer
-          className={this.props.data.themeName}
-          icon="info-sign"
-          onClose={this.handleClose}
-          title="Palantir Foundry"
-          {...this.state}
-        >
-          <div className={Classes.DRAWER_BODY}>
-            <div className={Classes.DIALOG_BODY}>
-              <p>
-                <strong>
-                  Data integration is the seminal problem of the digital age.
-                  For over ten years, we’ve helped the world’s premier
-                  organizations rise to the challenge.
-                </strong>
-              </p>
-              <p>
-                Palantir Foundry radically reimagines the way enterprises
-                interact with data by amplifying and extending the power of data
-                integration. With Foundry, anyone can source, fuse, and
-                transform data into any shape they desire. Business analysts
-                become data engineers — and leaders in their organization’s data
-                revolution.
-              </p>
-              <p>
-                Foundry’s back end includes a suite of best-in-class data
-                integration capabilities: data provenance, git-style versioning
-                semantics, granular access controls, branching, transformation
-                authoring, and more. But these powers are not limited to the
-                back-end IT shop.
-              </p>
-              <p>
-                In Foundry, tables, applications, reports, presentations, and
-                spreadsheets operate as data integrations in their own right.
-                Access controls, transformation logic, and data quality flow
-                from original data source to intermediate analysis to
-                presentation in real time. Every end product created in Foundry
-                becomes a new data source that other users can build upon. And
-                the enterprise data foundation goes where the business drives
-                it.
-              </p>
-              <p>
-                Start the revolution. Unleash the power of data integration with
-                Palantir Foundry.
-              </p>
-            </div>
+  return (
+    <Drawer isOpen size={Drawer.SIZE_SMALL}>
+      <div style={{ height: '100vh', backgroundColor: 'lightblue' }}>
+        <div>
+          <h3>PIXELEARN</h3>
+          <img src={user.picture} alt="profile" />
+          <h2>
+            {user.firstName} {user.lastName}{' '}
+          </h2>
+          <h2>{user.classroom}</h2>
+        </div>
+        <div>
+          <h2>FLUX</h2>
+          <div>
+            <Notification />
           </div>
-          <div className={Classes.DRAWER_FOOTER}>Footer</div>
-        </Drawer>
-      </Example>
-    );
-  }
-
-  private renderOptions() {
-    const {
-      autoFocus,
-      enforceFocus,
-      canEscapeKeyClose,
-      canOutsideClickClose,
-      hasBackdrop,
-      usePortal,
-    } = this.state;
-    return (
-      <>
-        <H5>Props</H5>
-        <Label>
-          Position
-          <HTMLSelect
-            value={this.state.position}
-            onChange={this.handlePositionChange}
-            options={VALID_POSITIONS}
-          />
-        </Label>
-        <Label>
-          Size
-          <HTMLSelect options={SIZES} onChange={this.handleSizeChange} />
-        </Label>
-        <Divider />
-        <Switch
-          checked={autoFocus}
-          label="Auto focus"
-          onChange={this.handleAutoFocusChange}
-        />
-        <Switch
-          checked={enforceFocus}
-          label="Enforce focus"
-          onChange={this.handleEnforceFocusChange}
-        />
-        <Switch
-          checked={hasBackdrop}
-          label="Has backdrop"
-          onChange={this.handleBackdropChange}
-        />
-        <Switch checked={usePortal} onChange={this.handleUsePortalChange}>
-          Use <Code>Portal</Code>
-        </Switch>
-        <Switch
-          checked={canOutsideClickClose}
-          label="Click outside to close"
-          onChange={this.handleOutsideClickChange}
-        />
-        <Switch
-          checked={canEscapeKeyClose}
-          label="Escape key to close"
-          onChange={this.handleEscapeKeyChange}
-        />
-      </>
-    );
-  }
-
-  private handleOpen = () => this.setState({ isOpen: true });
-
-  private handleClose = () => this.setState({ isOpen: false });
+        </div>
+        <button type="button">Déconnexion</button>
+      </div>
+    </Drawer>
+  );
 }
 
-const SIZES: Array<string | OptionProps> = [
-  { label: 'Default', value: undefined },
-  { label: 'Small', value: Drawer.SIZE_SMALL },
-  { label: 'Standard', value: Drawer.SIZE_STANDARD },
-  { label: 'Large', value: Drawer.SIZE_LARGE },
-  '72%',
-  '560px',
-];
-
-const VALID_POSITIONS: Position[] = [
-  Position.TOP,
-  Position.RIGHT,
-  Position.BOTTOM,
-  Position.LEFT,
-];
+export default Sidebar;
