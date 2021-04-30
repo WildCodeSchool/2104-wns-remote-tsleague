004b80adcda75231970db67e0fd2e141dc51da17
J <jonathan.arnould33@gmail.com>
2021-04-30 10:03:14 +0200

    feat(integrate-phaser): fix MenuInGame CSS

--- client/src/components/MenuInGame.tsx
+++ client/src/components/MenuInGame.tsx
@@ -3,25 +3,30 @@ import { Classes, Icon, Menu, MenuDivider, MenuItem } from '@blueprintjs/core';
 
 function MenuInGame(): JSX.Element {
   return (
-    <div style={{ width: '1px'}}>
-      <Menu className={Classes.ELEVATION_1}>
-        <MenuDivider />
-        <MenuItem icon="timeline-events" text="Agenda" />
-        <MenuItem icon="learning" text="Classe" />
-        <MenuItem icon="folder-open" text="Mes documents">
-          <MenuItem icon="annotation" text="Mes notes" />
-          <MenuItem icon="video" text="Mes videos" />
-          <MenuItem icon="link" text="Mes ressources" />
-        </MenuItem>
-        <MenuDivider />
-        <MenuItem
-          icon="cog"
-          labelElement={<Icon icon="share" />}
-          text="Paramètres"
-        />
-        <MenuItem icon="log-out" text="Déconnecter" />
-      </Menu>
-    </div>
+    <Menu
+      style={{
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+        left: '50%',
+      }}
+    >
+      <MenuDivider />
+      <MenuItem icon="timeline-events" text="Agenda" />
+      <MenuItem icon="learning" text="Classe" />
+      <MenuItem icon="folder-open" text="Mes documents">
+        <MenuItem icon="annotation" text="Mes notes" />
+        <MenuItem icon="video" text="Mes videos" />
+        <MenuItem icon="link" text="Mes ressources" />
+      </MenuItem>
+      <MenuDivider />
+      <MenuItem
+        icon="cog"
+        labelElement={<Icon icon="share" />}
+        text="Paramètres"
+      />
+      <MenuItem icon="log-out" text="Déconnecter" />
+    </Menu>
   );
 }
 
