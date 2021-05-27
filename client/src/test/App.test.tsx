2cef0354386947f57cb694c034a66c1229a0a71b
lucas <lucasleproust@gmail.com>
2021-05-27 13:27:46 +0200

    styled-components: add test for App

--- client/src/test/App.test.tsx
+++ client/src/test/App.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from '../components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../redux/root-reducer';
 
-test('renders learn react link', () => {
-  render(<App />);
+export function createTestStore() {
+  const store = createStore(rootReducer);
+  return store;
+}
+
+test('renders learn react link', async () => {
+  const store = createTestStore();
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  await waitFor(() => screen.getByTestId('app'));
   expect(screen.queryByTestId('app')).not.toBeNull();
-  // expect(screen.queryByTestId('app')).toBeInstanceOf(HTMLDivElement);
+  expect(screen.queryByTestId('app')).toBeInstanceOf(HTMLDivElement);
 });
