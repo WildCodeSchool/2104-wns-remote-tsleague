31acf146c03627a4f0a0a6b25cfc9799818da678
lucas <lucasleproust@gmail.com>
2021-05-27 13:41:57 +0200

    styled-components: code clean App test

--- client/src/test/App.test.tsx
+++ client/src/test/App.test.tsx
@@ -2,16 +2,9 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import App from '../components/App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from '../redux/root-reducer';
-
-export function createTestStore() {
-  const store = createStore(rootReducer);
-  return store;
-}
+import store from '../redux/store';
 
 test('renders learn react link', async () => {
-  const store = createTestStore();
   render(
     <Provider store={store}>
       <App />
