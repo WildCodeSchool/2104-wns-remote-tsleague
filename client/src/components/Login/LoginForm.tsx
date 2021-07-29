12486469b43bd627e75e70c9691cb7fe4ef7129c
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2021-07-29 15:50:19 +0200

    Implementation of Formik/Yup login with a customized style.
    Co-authored-by: lucas240 <lucas240@users.noreply.github.com>

--- client/src/components/Login/LoginForm.tsx
+++ client/src/components/Login/LoginForm.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
-
-import { StyledBox, StyledForm, StyledInput } from '../styles/Login';
+import * as Yup from 'yup';
+import { Formik, Form, Field } from 'formik';
+import { StyledBox } from '../styles/Login';
 import Button from '../common/Button';
+import Input from '../common/Input';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .min(4, 'Votre entrée est trop courte!')
+    .email('Veuillez mettre votre email')
+    .required('Ce champ est obligatoire'),
+  password: Yup.string()
+    .min(4, 'Votre entrée est trop courte!')
+    .required('Ce champ est obligatoire'),
+});
 
 function LoginForm(): JSX.Element {
   return (
     <StyledBox>
-      <StyledForm>
-        <StyledInput type="email" placeholder="Mail" />
-        <StyledInput type="password" placeholder="Mot de passe" />
-      </StyledForm>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Input
+              name="email"
+              type="email"
+              errors={errors.email}
+              touched={touched.email}
+              placeholder="Votre e-mail"
+            />
+            <Input
+              name="password"
+              type="password"
+              errors={errors.password}
+              touched={touched.password}
+              placeholder="Votre mot de passe"
+            />
+            {/* <button type="submit">Submit</button> */}
+            <Button text="Connectez-vous" type="submit" buttonStyle="submit" />
+          </Form>
+        )}
+      </Formik>
       <a href="http://localhost:3000/">Vous avez oublié votre mot de passe ?</a>
-      <Button
-        text="Connectez-vous"
-        buttonStyle="submit"
-        handleClick={() => console.log('connexion')}
-      />
     </StyledBox>
   );
 }
