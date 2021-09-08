95f47189edaf90b05efb996828fffaaf25887905
lucas <lucasleproust@gmail.com>
2021-09-08 14:51:37 +0200

    feat: server, add CRUD request for users resolver and add UserInput for validation

--- server/src/resolvers/users-resolver.ts
+++ server/src/resolvers/users-resolver.ts
@@ -1,19 +1,17 @@
 /* eslint-disable class-methods-use-this */
 import { Resolver, Query, Arg, Mutation } from 'type-graphql';
 import { UserModel, User } from '../models/users-model';
-
+import UserInput from './validator/userInput';
 // TODO => Handle error
 @Resolver(User)
 class UserResolver {
-  // cette méthode est une query, çàd qu'elle va servir à la lecture de données
-  // elle retourne tous les documents de type User
   @Query(() => [User])
   getAllUsers() {
     return UserModel.find();
   }
 
   @Query(() => User)
-  getUser(@Arg('id', {}) id: string) {
+  getUserById(@Arg('id', {}) id: string) {
     return UserModel.findById(id);
   }
 
@@ -22,61 +20,31 @@ class UserResolver {
     return UserModel.find({ role });
   }
 
-  // cette méthode est une mutation, çàd qu'elle va servir à écrire /modifier/supprimer de la donnée
-  // elle retourne le User nouvellement crée
-  // @Mutation(() => User)
-  // public async createUser(
-  //   @Arg('lastname') lastname: string,
-  //   @Arg('firstname') firstname: string,
-  //   @Arg('birthday') birthday: string,
-  //   @Arg('mail') mail: string,
-  //   @Arg('password') password: string,
-  //   @Arg('role') role: string,
-  //   @Arg('classrooms') classrooms: string[],
-  // ) {
-  //   await UserModel.init();
-  //   const body: User = {
-  //     lastname,
-  //     firstname,
-  //     birthday,
-  //     mail,
-  //     password,
-  //     role,
-  //     classrooms,
-  //   };
-  //   const model = new UserModel(body);
-  //   const result = await model.save();
-  //   return result;
-  // }
   @Mutation(() => User)
-  async createUser(
-    @Arg('lastname') lastname: string,
-    @Arg('firstname') firstname: string,
-    @Arg('birthday') birthday: string,
-    @Arg('mail') mail: string,
-    @Arg('password') password: string,
-    @Arg('role') role: string,
-    @Arg('classrooms') classrooms: string[],
-  ) {
-    const user = await UserModel.create({
-      lastname,
-      firstname,
-      birthday,
-      mail,
-      password,
-      role,
-      classrooms,
-    });
+  public async createUser(@Arg('input') userInput: UserInput) {
+    await UserModel.init();
+    const user = new UserModel(userInput);
+    await user.save();
+    return user;
+  }
+
+  @Mutation(() => User)
+  public async updateUser(@Arg('input') userInput: UserInput) {
+    const user = await UserModel.findOneAndUpdate(
+      { _id: userInput.id },
+      userInput,
+      {
+        new: true,
+      },
+    );
+    return user;
+  }
+
+  @Mutation(() => User, { nullable: true })
+  public async deleteUser(@Arg('id', () => String) id: string) {
+    const user = await UserModel.findOneAndDelete({ _id: id });
     return user;
   }
 }
 
 export default UserResolver;
-
-// "lastname": "john",
-// "firstname": "doe",
-// "birthday": "12/02/94",
-// "mail": "johndoe@tes.com",
-// "password": "test",
-// "role": "student",
-// "classrooms": [],
