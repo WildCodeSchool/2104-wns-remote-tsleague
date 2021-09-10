27589739271980b936b24db19c9e1d74a943012e
lucas <lucasleproust@gmail.com>
2021-09-10 11:37:19 +0200

    fix: add StyleSheet in Slide

--- mobile/screens/home/Slide.tsx
+++ mobile/screens/home/Slide.tsx
@@ -1,20 +1,24 @@
 import React from "react";
-import { Text, View, Image, Dimensions } from "react-native";
+import { Text, View, Dimensions, StyleSheet } from "react-native";
 
 const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
 
 export default function Slide({ data }: any) {
   return (
-    <View
-      style={{
-        width: windowWidth - 80,
-        height: windowHeight - 80,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 24, margin: 10 }}>{data.title}</Text>
-      <Text style={{ fontSize: 18 }}>{data.subtitle}</Text>
+    <View style={styles.slide}>
+      <Text style={styles.title}>{data.title}</Text>
+      <Text style={styles.subtitle}>{data.subtitle}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  slide: {
+    width: windowWidth - 80,
+    height: windowHeight - 80,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: { fontSize: 24, margin: 10 },
+  subtitle: { fontSize: 18 },
+});
