02e563172971aec5f3b5d6ec33fbe8d9584347e8
lucas <lucasleproust@gmail.com>
2021-09-10 16:25:17 +0200

    feat: wip graphql  apollo/client

--- mobile/screens/myAccount/MyAccountForm.tsx
+++ mobile/screens/myAccount/MyAccountForm.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Button, TextInput, View, Text } from "react-native";
 import { globalStyles } from "../../styles/global";
 import { ErrorMessage, Form, Formik } from "formik";
 import * as yup from "yup";
+import { gql, useMutation } from "@apollo/client";
 
 const ClassroomSchema = yup.object({
   name: yup
@@ -27,15 +28,25 @@ const ClassroomSchema = yup.object({
     .required("Ce champ est obligatoire"),
 });
 
+const CLASSROOM_MUTATION = gql`
+  mutation Chapters {
+    chapters {
+      id
+      number
+      title
+    }
+  }
+`;
+
 export default function MyAccountForm({ navigation }: any) {
   return (
     <View>
       <Formik
         initialValues={{
-          name: "",
-          email: "",
-          password: "",
-          confirmPassword: "",
+          name: "Johanne",
+          email: "johannelee@gmail.com",
+          password: "helloworld",
+          confirmPassword: "helloworld",
         }}
         validationSchema={ClassroomSchema}
         onSubmit={(values) => {
