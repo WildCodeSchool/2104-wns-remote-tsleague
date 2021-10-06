c624413b91f01ce9d574ac5ffceb9251c7872e11
Auxence Blondel <49131448+AuxB@users.noreply.github.com>
2021-10-06 12:31:11 +0200

    Fix sh problem (#17)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update deploy_prod.sh
    
    * Update README.md
    
    * Update deploy_staging.sh
    
    Co-authored-by: JonathanArnould <70904349+JonathanArnould@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Hello
 World
+!!!!!!
+v07
--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "PORT=$PORT"
 git fetch origin && git reset --hard origin/dev && git clean -f -d
 GATEWAY_PORT=$PORT docker-compose -f docker-compose.prod.yml up --build -d
