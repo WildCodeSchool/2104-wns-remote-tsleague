70d1cb911aefe7a9212bf8ba147bed9d301ebb5f
auxb <auxence.blondel@gmail.com>
2021-11-30 14:51:35 +0100

    feat(): add login and register mutation method

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,19 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+  ApolloLink,
+  useQuery,
+  gql,
+} from '@apollo/client';
+
 import Router from './components/Router';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
+
 import 'normalize.css';
+
+const link = createHttpLink({
+  uri: '/graphql',
+  credentials: 'include',
+});
+
+const authMiddleware = new ApolloLink((operation): any => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || null,
+    },
+  }));
+});
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5050',
+  cache: new InMemoryCache(),
+  link,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
