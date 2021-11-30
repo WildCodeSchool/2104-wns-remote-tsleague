d2d7688964bd77bc7f223958b7047a1a5849a53c
JonathanArnould <jonathan.arnould33@gmail.com>
2021-11-30 10:52:21 +0100

    check time phase scene

--- client/src/redux/middleware/socket-middleware.ts
+++ client/src/redux/middleware/socket-middleware.ts
@@ -14,10 +14,6 @@ const buildSocketMiddleware: (socket: Socket) => Middleware =
           action.payload.positionY !== prevPosition.positionY
         ) {
           socket.emit('STUDENT_GAME_POSITION', action.payload);
-          // socket.on('STUDENT_GAME_POSITION', (payload) => {
-            // console.log(`position X: ${payload.positionX}, 
-            // positionY: ${payload.positionY}`);
-          // });
         }
         break;
       default:
